63b4128b66fcc37180eb7ba77275b225ac28f6eb
sarahwilson <sarahwilson608@gmail.com>
2019-05-21 17:15:05 +0100

    random team name
    changed js file and index file to give a random team name

--- js/background.js
+++ js/background.js
@@ -2,30 +2,37 @@
 console.log('hello!')
 
 // three different variables for the different images
-var backgrounds = ['mike-yukhtenko', 'gabriele-garanzelli', 'fabian-oelkers']
+var backgrounds = ['Team Hello World','Team Callback Cats','Team The Four Fridgeteers','Team Pastabilities','Team The Beige Cardis','Team Attenborough', 'Team ZKhenna']
 
 // Only fires logic after the page has fully loaded
 $( document ).ready(function(){
-
 
   // when the button is clicked it triggers a function
   $('h1').on('click', function () {
 
     // 1. log to the console that you have clicked the button
+    console.log('I was clicked!')
 
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     // this creates a string that css will understand to be a background url
     var imageUrl = "url('./images/" + randomBackground + ".jpg')"
-    
-    
+
+
     // 2. using jquery, change the image url of the body background to the new one saved above
+    $('body').css('background', imageUrl)
+
+
 
     // EXTENSION TASK!
     // 3. Change the text of the 'span' to be that of the photographer
-    //   (hint, you should re-use randomBackgroun)
-    // Double points if you replace '-' between their names with a space
+    //   (hint, you should re-use randomBackground)
+    $('h1').text(randomBackground)
 
-    
-  }) 
-}) // here endeth the function
+    // Double points if you replace '-' between their names with a space
+    var artist = randomBackground.replace(/-/g, ' ');
+    $('span').text(artist)
+
+
+      })
+    }) // here endeth the function
